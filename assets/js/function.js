8c4b143f1dc4d85b75ef87faf6711540e59e0fa1
Szabó Balázs <szabo.balazs2017@gmail.com>
2020-11-06 21:12:31 +0200

    Design synergies

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,3 +1,23 @@
+---
+---
+
 $(document).ready(function(){
-  
+
+  $("#class-origins-section .adept").show();
+  $("#class-origins-section .adept").addClass("active");
+  $("#class-origins-section .synergy-icons #adept").addClass("active");
+
+  {% for item in site.data.synergies %}
+  $("#class-origins-section .synergy-icons #{{ item.name | split:' '}} ").click(function(){
+    if (!$(this).hasClass("active")) {
+      $("#class-origins-section .synergy-container .active").hide(300);
+      $("#class-origins-section .active").removeClass("active");
+
+      $("#class-origins-section .{{ item.name | split:' '}} ").show(300);
+      $("#class-origins-section .{{ item.name | split:' '}} ").addClass("active");
+      $(this).addClass("active");
+    }
+  });
+  {% endfor %}
+
 });
